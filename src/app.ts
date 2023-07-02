d51c5d7fe7de72c221a46bd35754e5a59346ab56
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-07-02 16:22:50 +0200

    Update to optional port environment variable

--- src/app.ts
+++ src/app.ts
@@ -58,7 +58,7 @@ async function scrapeMenu() {
   return menu
 }
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.clear()
   console.log(`Server is running on http://localhost:${port}`)
