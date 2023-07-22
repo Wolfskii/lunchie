54e67613aa16815424cc3619fdbab51300191c54
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-07-22 19:09:18 +0200

    Add the personal response type instead

--- src/discord/commands/menu.ts
+++ src/discord/commands/menu.ts
@@ -7,13 +7,15 @@ import { getTodaysMenu } from '../../utils/menuScraper'
 export const today: Command = {
   data: new SlashCommandBuilder().setName('today').setDescription(`Get today's lunch-menu options`),
   async execute(interaction: CommandInteraction) {
-    // await postTodaysMenuToDiscord(interaction.client)
     // await interaction.deferReply()
-    // await interaction.reply('Halloj') // Response visible for all users in the channel
 
     getTodaysMenu().then((todaysMenu: string) => {
       interaction.reply({ content: todaysMenu, ephemeral: true }) // Response only visible to executing user
     })
+
+    /* getTodaysMenu().then((todaysMenu: string) => {
+      interaction.reply(todaysMenu) // Response visible for all users in the channel
+    }) */
 
     // interaction.editReply('Pong!') // To edit the message afterwards
   }
